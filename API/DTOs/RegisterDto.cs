dffc38db0590d3f42cb3257db7546f9d154693c0
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-09-08 19:57:17 -0400

    Section 7 Complete

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,7 +10,9 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+
         [Required]
+        [StringLength(16,MinimumLength =8)]
         public string Password { get; set; }
     }
 }
